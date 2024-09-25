aaefff1c88f848494eead27d1e8f0e20cbf09d87
universe119 <ruddh119@gmail.com>
2024-09-25 10:47:39 +0900

    data-커스텀속성으로 정보값을 연결하고 e.target으로 이벤트가 발생한 대상의 속성에 담아놓은 정보값 가져와 활용

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,11 @@
 // innerHTML을 활용한 동적 DOM생성
 const frame = document.querySelector("section");
-const data = ["title1", "title2", "title3"];
+// const data = ["title1", "title2", "title3"];
+const data = [
+	{ text: "title", bg: "hotpink" },
+	{ text: "title2", bg: "aqua" },
+	{ text: "title3", bg: "orange" },
+];
 
 let tags = "";
 
@@ -8,7 +13,7 @@ let tags = "";
 data.forEach((el) => {
 	tags += `
     <article>
-      <h1>${el}</h1>
+      <h1 class="tit" data-bg=${el.bg}>${el.text}</h1>
     </article>
   `;
 });
@@ -24,8 +29,25 @@ asideEl.innerText = "Modal";
 //aside요소 안쪽의 복잡한 자식 요소 구조는 innerHTML로 생성
 asideEl.innerHTML = `
   <div class="con"></div>
-  <button>CLOSE</button>
+  <button class="btnClose">CLOSE</button>
 `;
 
 // 자식 요소까지 적용된 aside요소를 기존 자식요소를 윶한 상태에서 추가
-document.body.append(asideEl);
+
+// 동적으로 생성된 요소에 이벤트 연결은 이벤트 위임 처리
+// 이벤트 위임: 항상 존재하는 body요소에 이벤트를 맡겼다가 위임처리
+document.body.addEventListener("click", (e) => {
+	// console.log(e.target);
+	// 내가 화면상에 클릭한 요소가 .tit클래스를 가진 요소일때에만 모달창 생성
+	if (e.target.className === "tit") {
+		document.body.append(asideEl);
+		asideEl.style.backgroundColor = e.target.getAttribute("data-bg");
+	}
+});
+
+// DOM요소를 완전히 제거 (DOM트리에서 제거)
+document.body.addEventListener("click", (e) => {
+	if (e.target.className === "btnClose") {
+		document.querySelector(".modal").remove();
+	}
+});
